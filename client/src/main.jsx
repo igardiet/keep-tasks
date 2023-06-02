dec744bf2f7b1f62ebbe8f225d97c83a24f96e54
Iñaki García <igardiet@gmail.com>
2023-06-02 23:43:10 +0200

    FEAT: context barrel import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { TasksContextProvider, AuthContextProvider } from './context';
 import './styles/styles.css';
-import { TasksContextProvider } from './context/TaskContext';
-import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
