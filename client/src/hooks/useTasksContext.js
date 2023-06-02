501458118457c19cb29ae0e1fe4cb8d230553cef
Iñaki García <igardiet@gmail.com>
2023-06-02 23:45:48 +0200

    FEAT: context barrel import

--- client/src/hooks/useTasksContext.js
+++ client/src/hooks/useTasksContext.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { TasksContext } from '../context/TaskContext';
+import { TasksContext } from '../context';
 
 export const useTasksContext = () => {
   const context = useContext(TasksContext);
