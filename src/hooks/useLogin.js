5e110e79a18f953f400cd72c78862d7048b387bb
Iñaki García <igardiet@gmail.com>
2023-06-07 16:56:07 +0200

    restore

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -11,7 +11,7 @@ export const useLogin = () => {
     setError(null);
 
     const response = await fetch(
-      `http://localhost:3000/api/user/login`,
+      `${import.meta.env.VITE_APP_API_URL}/api/user/login`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
